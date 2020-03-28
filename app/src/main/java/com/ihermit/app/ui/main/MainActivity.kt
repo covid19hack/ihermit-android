a79d969ffed46935185f57f37de6c0de5a97c38d
Malvin Sutanto <malvinsutanto@gmail.com>
2020-03-28 16:09:25 +0900

    Basic sample of getting location with permission

--- app/src/main/java/com/ihermit/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/ihermit/app/ui/main/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.ihermit.app
+package com.ihermit.app.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ihermit.app.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.ihermit.app.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/com/ihermit/app/ui/main/MainFragment.kt
+++ app/src/main/java/com/ihermit/app/ui/main/MainFragment.kt
@@ -1,16 +1,32 @@
 package com.ihermit.app.ui.main
 
+import android.Manifest
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.LocationServices
 import com.ihermit.app.R
+import com.ihermit.app.databinding.MainFragmentBinding
 import dagger.android.support.DaggerFragment
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.ktx.withPermissionsCheck
 import javax.inject.Inject
 
-class MainFragment : DaggerFragment() {
+
+private val PERMISSIONS: Array<String> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    )
+} else {
+    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+}
+
+class MainFragment : DaggerFragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -21,16 +37,39 @@ class MainFragment : DaggerFragment() {
 
     private val viewModel by viewModels<MainViewModel> { viewModelFactory }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        MainFragmentBinding.bind(view).setup()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+    private fun MainFragmentBinding.setup() {
+        location.setOnClickListener {
+            withPermissionsCheck(
+                *PERMISSIONS,
+                onShowRationale = ::showLocationRationale,
+                onPermissionDenied = ::retryLocationRequest,
+                onNeverAskAgain = ::showNeedsLocation
+            ) {
+                LocationServices.getFusedLocationProviderClient(requireActivity())
+                    .lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        Toast.makeText(requireActivity(), "Received location ", Toast.LENGTH_LONG)
+                            .show()
+                    }
+            }
+        }
+    }
+
+    private fun showNeedsLocation() {
+        Toast.makeText(requireActivity(), "Needs location to work.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun retryLocationRequest() {
+        Toast.makeText(requireActivity(), "Please don't deny.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun showLocationRationale(permissionRequest: PermissionRequest) {
+        permissionRequest.proceed()
     }
 
 }
