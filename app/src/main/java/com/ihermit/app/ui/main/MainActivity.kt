7b7e126f523beda61fd4cf08cbe5694b64635dd3
Malvin Sutanto <malvinsutanto@gmail.com>
2020-03-29 13:07:25 +0900

    Use nav comp in main activity

--- app/src/main/java/com/ihermit/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/ihermit/app/ui/main/MainActivity.kt
@@ -2,17 +2,12 @@ package com.ihermit.app.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ihermit.app.R
+import com.ihermit.app.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setContentView(MainActivityBinding.inflate(layoutInflater).root)
     }
 }
