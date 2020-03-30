1a06d27e276727aefcef3bff0855a8a9c1a3bb6f
Malvin Sutanto <malvinsutanto@gmail.com>
2020-03-30 19:51:25 +0900

    Updates API

--- app/src/main/java/com/ihermit/app/data/network/HermitService.kt
+++ app/src/main/java/com/ihermit/app/data/network/HermitService.kt
@@ -17,6 +17,6 @@ interface HermitService {
     @PATCH("users/updateNickName")
     suspend fun updateNickName(@Body request: UpdateUserRequestBody)
 
-    @POST("users/createCheckIn")
+    @POST("users/checkIn")
     suspend fun checkIn(@Body request: CheckInRequest): User
 }
